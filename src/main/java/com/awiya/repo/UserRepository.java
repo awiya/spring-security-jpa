210542cb3512fcfcefbfb4e818ca693dd2d76521
AYOUB <a.idihia@gmail.com>
2023-03-24 15:10:12 +0100

    first commit

---
+++ src/main/java/com/awiya/repo/UserRepository.java
@@ -0,0 +1,11 @@
+package com.awiya.repo;
+
+
+import com.awiya.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUserName(String userName);
+}
